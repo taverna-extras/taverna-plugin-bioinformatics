c698ec61bde4244b375b43842eabd47aad1b678a
edward.kawas@gmail.com <edward.kawas@gmail.com@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-04-03 15:44:10 +0000

    added a call to GetOntologyThread to download the ontology that a particular service came from (to speed up service execution).
    git-svn-id: http://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.activities/trunk/biomoby-activity@7209 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/activities/biomoby/BiomobyActivity.java
+++ src/main/java/net/sf/taverna/t2/activities/biomoby/BiomobyActivity.java
@@ -977,6 +977,13 @@ public class BiomobyActivity extends
 		} catch (Exception e) {
 			/* don't care if an exception occurs here ... */
 		}
+		// here we make sure that we have downloaded the ontology for the
+		// registry that we got this service from
+		try {
+			new GetOntologyThread(worker.getRegistryEndpoint()).start();
+		} catch (Exception e) {
+			/* don't care if an exception occurs here ... */
+		}
 
 	}
 
