3cb78d9cabb0c3ce306f195392758327134187b1
edward.kawas@gmail.com <edward.kawas@gmail.com@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-06-04 13:24:28 +0000

    removed unused import
    git-svn-id: http://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-activities/trunk/biomoby-activity-ui@7772 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/activities/biomoby/actions/AddParserActionHelper.java
+++ src/main/java/net/sf/taverna/t2/activities/biomoby/actions/AddParserActionHelper.java
@@ -10,7 +10,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
