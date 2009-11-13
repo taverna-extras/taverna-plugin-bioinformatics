d68e5f0566b518ecae828cbba8760268e253c009
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-11-13 14:33:43 +0000

    Changed "activity" to "service" as per T2-446
    git-svn-id: http://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-activities/trunk/biomart-activity-ui@9089 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/activities/biomart/views/BiomartActivityContextualView.java
+++ src/main/java/net/sf/taverna/t2/activities/biomart/views/BiomartActivityContextualView.java
@@ -66,7 +66,7 @@ public class BiomartActivityContextualView extends HTMLBasedActivityContextualVi
 
 	@Override
 	public String getViewTitle() {
-		return "Biomart activity";
+		return "Biomart service";
 	}
 
 	@SuppressWarnings("serial")
