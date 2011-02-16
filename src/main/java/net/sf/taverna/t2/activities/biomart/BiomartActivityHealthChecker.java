8feb6dbd71bbe35edaf2c12f0313712bb42b4860
alex@mygrid.org.uk <alex@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-02-16 14:55:33 +0000

    Removed some unused imports.
    git-svn-id: http://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.activities/branches/maintenance/biomart-activity@12602 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/activities/biomart/BiomartActivityHealthChecker.java
+++ src/main/java/net/sf/taverna/t2/activities/biomart/BiomartActivityHealthChecker.java
@@ -20,19 +20,9 @@
  ******************************************************************************/
 package net.sf.taverna.t2.activities.biomart;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.SocketTimeoutException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.List;
 
-import net.sf.taverna.t2.workflowmodel.Processor;
-import net.sf.taverna.t2.workflowmodel.health.HealthCheck;
-import net.sf.taverna.t2.workflowmodel.health.HealthChecker;
 import net.sf.taverna.t2.visit.VisitReport;
-import net.sf.taverna.t2.visit.VisitReport.Status;
 
 import net.sf.taverna.t2.workflowmodel.health.RemoteHealthChecker;
 import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
