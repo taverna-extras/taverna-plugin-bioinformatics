31fdced86c835899a925cafab612862f79e763a4
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-09-05 14:15:11 +0100

    Avoid repositories starting with #

--- clone.sh
+++ clone.sh
@@ -3,9 +3,9 @@ set -e
 
 
 if [ "$1" == "--ssh" ] ; then  
-	(for a in $(cat repos) ; do echo git@github.com:taverna/$a.git ; done) | xargs --max-procs=5 --max-args=1 git clone
+	(for a in $(grep -v ^# repos) ; do echo git@github.com:taverna/$a.git ; done) | xargs --max-procs=5 --max-args=1 git clone
 else
-	(for a in $(cat repos) ; do echo https://github.com/taverna/$a.git ; done) | xargs --max-procs=5 --max-args=1 git clone
+	(for a in $(grep -v ^# repos) ; do echo https://github.com/taverna/$a.git ; done) | xargs --max-procs=5 --max-args=1 git clone
 fi
 
 
