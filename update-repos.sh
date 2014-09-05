4791476a796c68f1c6df77980b718d2102a6fcc9
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-09-05 14:28:28 +0100

    Add ignores to update-repos.sh

--- update-repos.sh
+++ update-repos.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 # Not exactly compliant JSON parsing here.. also assume no more than 200 repositories
-curl -L -H "Accept: application/vnd.github.v3+json" 'https://api.github.com/orgs/taverna/repos?per_page=100&page=[1-2]' | grep \"name\" | cut -d '"' -f 4 | sort > repos
+
+URI='https://api.github.com/orgs/taverna/repos?per_page=100&page=[1-2]'
+
+curl -L -H "Accept: application/vnd.github.v3+json" "$URI" | 
+  grep \"name\" | cut -d '"' -f 4 | 
+  grep -v taverna2- | grep -v taverna-build | sort > repos
