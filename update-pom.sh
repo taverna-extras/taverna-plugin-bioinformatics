3ee0098b7fc740d53ce89db25d1f783b4db1bbac
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-09-05 14:45:14 +0100

    Source repos/ instead of */pom.xml
    thus honoring an updated repos.ignored

--- update-pom.sh
+++ update-pom.sh
@@ -2,8 +2,9 @@
 
 grep -B 100000 "modules inserted" pom.xml.template > pom.xml
 
-for pom in */pom.xml ; do
-	module=$(echo $pom|sed s,/pom.xml,,)
-	echo "<module>$module</module>" >> pom.xml
+for repo in $(cat repos) ; do
+  if [ -f $repo/pom.xml ] ; then
+    echo "<module>$repo</module>" >> pom.xml
+  fi
 done
 grep -A 100000 "modules inserted" pom.xml.template >> pom.xml
