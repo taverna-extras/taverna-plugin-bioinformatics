0b4fc376217ff04a8848ef27ff7d440e589ba9a4
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-09-05 16:53:07 +0100

    parallell git pull

--- pull.sh
+++ pull.sh
@@ -2,7 +2,9 @@
 set -e
 for a in */. ; do 
 	pushd $a 
-  git checkout master || true
-	git pull
-	popd
+  git checkout master >/dev/null || true
+	git pull >/dev/null &
+	popd >/dev/null
 done
+echo Waiting for git pull to complete..
+wait
