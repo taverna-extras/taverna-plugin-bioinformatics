d73dc9e694d88823b3e17c4c17a696e62dff7aa0
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-09-05 14:13:28 +0100

    list of repos is alphabetically sorted

--- update-repos.sh
+++ update-repos.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Not exactly compliant JSON parsing here.. also assume no more than 200 repositories
-curl -L -H "Accept: application/vnd.github.v3+json" 'https://api.github.com/orgs/taverna/repos?per_page=100&page=[1-2]' | grep \"name\" | cut -d '"' -f 4 > repos
+curl -L -H "Accept: application/vnd.github.v3+json" 'https://api.github.com/orgs/taverna/repos?per_page=100&page=[1-2]' | grep \"name\" | cut -d '"' -f 4 | sort > repos
